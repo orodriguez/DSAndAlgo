9340fa02cdfffb53217e3eecbc843ef511ef37c1
Omar Rodriguez <orodriguez@grubhub.com>
2023-08-14 19:19:49 -0400

    Use Theory in reverse problem

--- Tests/Stack/ReverseStringTest.cs
+++ Tests/Stack/ReverseStringTest.cs
@@ -5,19 +5,11 @@ using static ReverseString;
 
 public class ReverseStringTest
 {
-    [Fact]
-    public void Empty() => 
-        Assert.Equal("", Reverse(""));
-
-    [Fact]
-    public void OneChar() =>
-        Assert.Equal("a", Reverse("a"));
-
-    [Fact]
-    public void TwoChars() =>
-        Assert.Equal("ba", Reverse("ab"));
-    
-    [Fact]
-    public void ManyChars() =>
-        Assert.Equal("ihgfedcba", Reverse("abcdefghi"));
+    [Theory]
+    [InlineData("", "")]
+    [InlineData("a", "a")]
+    [InlineData("ab", "ba")]
+    [InlineData("abcd", "dcba")]
+    public void Test(string input, string result) =>
+        Assert.Equal(result, Reverse(input));
 }
